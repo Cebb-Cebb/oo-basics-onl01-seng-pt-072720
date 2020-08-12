a0265ce7f7b3742e02b951f90b7a47aed1d73c3a
hersson ceballos <herssonceballos@gmail.com>
2020-08-12 22:29:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,76 @@
-# Make your shoe class here!
+# # Make your shoe class here!
+require 'pry'
+class Shoe 
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def brand
+    @brand
+  end 
+  
+  def color=(red)
+    @color = red
+  end 
+  
+  def color
+    @color
+  end 
+  
+  def size=(num)
+    @size = num
+  end 
+  
+  def size
+    @size
+  end 
+  
+end 
+
+binding.pry
+
+
+# class Book
+  
+#   def initialize(title)
+#     @title = title
+#   end 
+  
+#   def title
+#     @title
+#   end 
+  
+#   def author=(author)
+#     @author = author
+#   end 
+  
+#   def author
+#     @author
+#   end 
+  
+#   def page_count=(num)
+#     @page_count = num 
+#   end 
+  
+#   def page_count
+#     @page_count
+#   end 
+  
+#   def genre=(genre)
+#     @genre = genre
+#   end 
+  
+#   def genre
+#     @genre
+#   end 
+  
+#   def turn_page 
+#     puts "Flipping the page...wow, you read fast!"
+#   end 
+  
+# end
+
+# Book.new("And Then There Were None")
+# book = Book.new("Some Title")
+# book.turn_page
