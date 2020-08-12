13fa6b7dc1defa61219f443ba379414199d71c93
hersson ceballos <herssonceballos@gmail.com>
2020-08-12 22:46:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -43,57 +43,11 @@ class Shoe
   end 
   
   def cobble
-    puts "new!"
+    puts "Your shoe is as good as new!"
   end 
   
 end 
 
-cobble = Shoe.new("new")
-cobble.condition
 
 
 
-
-# class Book
-  
-#   def initialize(title)
-#     @title = title
-#   end 
-  
-#   def title
-#     @title
-#   end 
-  
-#   def author=(author)
-#     @author = author
-#   end 
-  
-#   def author
-#     @author
-#   end 
-  
-#   def page_count=(num)
-#     @page_count = num 
-#   end 
-  
-#   def page_count
-#     @page_count
-#   end 
-  
-#   def genre=(genre)
-#     @genre = genre
-#   end 
-  
-#   def genre
-#     @genre
-#   end 
-  
-#   def turn_page 
-#     puts "Flipping the page...wow, you read fast!"
-#   end 
-  
-# end
-
-# Book.new("And Then There Were None")
-# book = Book.new("Some Title")
-# book.turn_page
